2df32ed8fe40965f188f92002cf470263d59c3f3
Andzej Mackevic <mackiewicz1995@gmail.com>
2024-09-02 21:57:51 +0200

    Update homework_6_2.py
    added pull request reviewer proposition - after merging branches together

--- lesson_6_2/homework_6_2.py
+++ lesson_6_2/homework_6_2.py
@@ -2,8 +2,7 @@ if_h_found = False
 while(True):
     entered_string = input("Please enter string which include 'h' or 'H' letter:")
     entered_string = entered_string.upper()
-    list_of_characters = entered_string[::1]
-    if(list_of_characters.count('H') <= 0):
+    if(entered_string.count('H') <= 0):
         print("There is not 'h' or 'H' letters! Please repeat!")
         continue
     else:
