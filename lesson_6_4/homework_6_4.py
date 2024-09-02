b06031187ea549034fe9868dab8e28a1327ee471
Andzej Mackevic <mackiewicz1995@gmail.com>
2024-09-02 21:53:32 +0200

    Update lesson_6_4/homework_6_4.py
    added reviewer proposition
    
    Co-authored-by: Eugene Danilovets <eugene.danilovets@gmail.com>

--- lesson_6_4/homework_6_4.py
+++ lesson_6_4/homework_6_4.py
@@ -2,7 +2,7 @@ list = [1, 4, 57, 79, 6, 9, 12, 7, 9, 11, 100 ]
 list2 = []
 
 for item in list:
-    if(item % 2 == 0):
+    if item % 2 == 0:
         list2.append((item))
 
 print(f"Sum of all even numbers is {sum(list2)}")
